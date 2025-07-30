a9449037d93eb5d519d41868771381235c33da93
err0rgod <ndgaming458@gmail.com>
2025-07-30 19:47:52 +0530

    ye -h ka kuch ho nhi sakta kya

--- basic.py
+++ basic.py
@@ -6,7 +6,7 @@ import argparse
 parser = argparse.ArgumentParser(description="Basic FTP tester")
 
 
-parser.add_argument("-h","--host",required=True, type=str, help="Enter the target to test ftp")
+parser.add_argument("-ho","--host",required=True, type=str, help="Enter the target to test ftp")
 parser.add_argument("-p","--passfile",required=True,type=str,help="Enter the path of file containing password list")
 parser.add_argument("-t","--threads",type=int,help="enter the number of threads")
 parser.add_argument("-u","--user",required=True,type=str,help="enter the Username to connect to provided host")
