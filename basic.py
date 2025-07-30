a01ab0c1a571c96ee4abf06c4ea940369d62648e
err0rgod <ndgaming458@gmail.com>
2025-07-30 19:43:25 +0530

    added args

--- basic.py
+++ basic.py
@@ -9,10 +9,15 @@ parser = argparse.ArgumentParser(description="Basic FTP tester")
 parser.add_argument("-h","--host",required=True, type=str, help="Enter the target to test ftp")
 parser.add_argument("-p","--passfile",required=True,type=str,help="Enter the path of file containing password list")
 parser.add_argument("-t","--threads",type=int,help="enter the number of threads")
+parser.add_argument("-u","--user",required=True,type=str,help="enter the Username to connect to provided host")
 
-host = "test.rebex.net"
-user = "demo"
-password = "password"
+args = parser.parse_args()
+
+
+
+host = args.host
+user = args.user
+passfile = args.passfile
 
 try:
     with ftplib.FTP() as serve:
